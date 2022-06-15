e31b7912fbb21a5c2a13fa6f0f65bf6ebe1794df
Stanley Neoh <95394480+StanleyNeoh@users.noreply.github.com>
2022-06-16 01:17:26 +0800

    Delete mock directory

--- mock/wn_user_group_mock.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE wn_user_group;
-
-CREATE TABLE wn_user_group (
-    user_id BIGINT REFERENCES wn_user(id) ON DELETE CASCADE,
-    group_id BIGINT REFERENCES wn_group(id) ON DELETE CASCADE,
-    unique(user_id, group_id)
-);
-
-GRANT ALL PRIVILEGES ON wn_user_group TO wellnus_admin;
